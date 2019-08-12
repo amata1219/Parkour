4839274121167709b582d54c1babbd91ddc9afa0
amata1219 <suzukiamata@gmail.com>
2019-08-12 13:17:28 +0900

    こみっとー

--- src/amata1219/parkour/Tweet.java
+++ src/amata1219/parkour/Tweet.java
@@ -1,7 +1,9 @@
 package amata1219.parkour;
 
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 
+import amata1219.amalib.sound.SoundMetadata;
 import amata1219.amalib.string.StringTemplate;
 import amata1219.amalib.string.message.Message;
 import amata1219.amalib.string.message.Message.ClickAction;
@@ -10,9 +12,13 @@ import amata1219.amalib.string.tweet.IntentTweet;
 
 public class Tweet {
 
+	private static final SoundMetadata SE = new SoundMetadata(Sound.ENTITY_PLAYER_LEVELUP, 1f, 1.75f);
+
 	public static void display(Player player, String text){
 		//テキストにアジ鯖のハッシュタグを追加してビルドする
 		String tweet = IntentTweet.write(text).addHashtag("アジ鯖").build();
+
+		SE.play(player);
 
 		Message.wrap(StringTemplate.capply("&b-# Share on Twitter &7-@ &f-$0", text)).displayAsClickableAndHoverable(player, ClickAction.OPEN_URL, tweet, HoverAction.SHOW_TEXT, "&7-: &b-Click to tweet!");
 	}
