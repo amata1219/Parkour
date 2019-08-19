9110cc8d1fcc3bdacfb0e6875c5e1cfd5a6f355e
amata1219 <suzukiamata@gmail.com>
2019-08-19 21:04:26 +0900

    こみっとー

--- src/amata1219/parkour/user/InformationBoard.java
+++ src/amata1219/parkour/user/InformationBoard.java
@@ -11,33 +11,31 @@ import org.bukkit.craftbukkit.v1_13_R2.entity.CraftPlayer;
 
 import amata1219.amalib.scoreboard.Scoreboard;
 import amata1219.amalib.string.StringColor;
-import amata1219.amalib.string.StringTemplate;
-import amata1219.amalib.string.message.Localizer;
 import amata1219.amalib.tuplet.Quadruple;
 
 public class InformationBoard {
 
-	private static final List<Quadruple<Function<UserSetting, Boolean>, Integer, Function<Localizer, String>, Function<User, Object>>> LINES = new ArrayList<>(12);
+	private static final List<Quadruple<Function<UserSetting, Boolean>, Integer, String, Function<User, Object>>> LINES = new ArrayList<>(12);
 
 	static{
 		initialize(
-			new Quadruple<>(s -> true, 11, l -> "", u -> ""),
-			new Quadruple<>(s -> true, 10, l -> "", u -> ""),
-			new Quadruple<>(s -> s.displayTraceur, 9, l -> l.color("&b-Traceur &7-@ &f-$0 | &b-トレイサー &7-@ &f-$0"), u -> u.asBukkitPlayer().getName()),
-			new Quadruple<>(s -> s.displayUpdateRank, 8, l -> l.color("&b-Update Rank &7-@ &f-$0 | &b-Updateランク &7-@ &f-$0"), u -> u.getUpdateRank()),
-			new Quadruple<>(s -> s.displayExtendRank, 7, l -> l.color("&b-Extend Rank &7-@ &f-$0 | &b-Extendランク &7-@ &f-$0"), u -> u.getExtendRank()),
-			new Quadruple<>(s -> s.displayJumps, 6, l -> l.color("&b-Jumps &7-@ &f-$0 | &b-ジャンプ回数 &7-@ &f-$0"), u -> u.asBukkitPlayer().getStatistic(Statistic.JUMP)),
-			new Quadruple<>(s -> s.displayCoins, 5, l -> l.color("&b-Coins &7-@ &f-$0 | &b-所持コイン数 &7-@ &f-$0"), u -> u.getCoins()),
-			new Quadruple<>(s -> s.displayTimePlayed, 4, l -> l.color("&b-Time Played &7-@ &f-$0h | &b-総プレイ時間 &7-@ &f-$0"), u -> u.asBukkitPlayer().getStatistic(Statistic.PLAY_ONE_MINUTE) / 72000),
-			new Quadruple<>(s -> s.displayOnlinePlayers, 3, l -> l.color("&b-Online Players &7-@ &f-$0 | &b-オンラインプレイヤー数 &7-@ &f-$0"), u -> Bukkit.getOnlinePlayers().size()),
-			new Quadruple<>(s -> s.displayPing, 2, l -> l.color("&b-Ping &7-@ &f-$0ms | &b-ピン &7-@ &f-$0"), u -> ((CraftPlayer) u.asBukkitPlayer()).getHandle().ping),
-			new Quadruple<>(s -> true, 1, l -> "", u -> ""),
-			new Quadruple<>(s -> s.displayServerAddress, 0, l -> l.color("&b-$0"), u -> "   azisaba.net")
+			new Quadruple<>(s -> true, 11, "", u -> ""),
+			new Quadruple<>(s -> true, 10, "", u -> ""),
+			new Quadruple<>(s -> s.displayTraceur, 9, "&b-トレイサー &7-@ &f-$0 | &b-Traceur &7-@ &f-$0", u -> u.asBukkitPlayer().getName()),
+			new Quadruple<>(s -> s.displayUpdateRank, 8, "&b-Updateランク &7-@ &f-$0 | &b-Update Rank &7-@ &f-$0", u -> u.getUpdateRank()),
+			new Quadruple<>(s -> s.displayExtendRank, 7, "&b-Extendランク &7-@ &f-$0 | &b-Extend Rank &7-@ &f-$0", u -> u.getExtendRank()),
+			new Quadruple<>(s -> s.displayJumps, 6, "&b-ジャンプ回数 &7-@ &f-$0 | &b-Jumps &7-@ &f-$0", u -> u.asBukkitPlayer().getStatistic(Statistic.JUMP)),
+			new Quadruple<>(s -> s.displayCoins, 5, "&b-所持コイン数 &7-@ &f-$0 | &b-Coins &7-@ &f-$0", u -> u.getCoins()),
+			new Quadruple<>(s -> s.displayTimePlayed, 4, "&b-総プレイ時間 &7-@ &f-$0 | &b-Time Played &7-@ &f-$0h", u -> u.asBukkitPlayer().getStatistic(Statistic.PLAY_ONE_MINUTE) / 72000),
+			new Quadruple<>(s -> s.displayOnlinePlayers, 3, "&b-オンラインプレイヤー数 &7-@ &f-$0 | &b-Online Players &7-@ &f-$0", u -> Bukkit.getOnlinePlayers().size()),
+			new Quadruple<>(s -> s.displayPing, 2, "&b-ピン &7-@ &f-$0 | &b-Ping &7-@ &f-$0ms", u -> ((CraftPlayer) u.asBukkitPlayer()).getHandle().ping),
+			new Quadruple<>(s -> true, 1, "", u -> ""),
+			new Quadruple<>(s -> s.displayServerAddress, 0, "&b-$0 | &b-$0", u -> "   azisaba.net")
 		);
 	}
 
 	@SafeVarargs
-	private static void initialize(Quadruple<Function<UserSetting, Boolean>, Integer, Function<Localizer, String>, Function<User, Object>>... components){
+	private static void initialize(Quadruple<Function<UserSetting, Boolean>, Integer, String, Function<User, Object>>... components){
 		Arrays.stream(components).forEach(LINES::add);
 	}
 
@@ -62,15 +60,16 @@ public class InformationBoard {
 		//スコアボードを新しく作成する
 		board = new Scoreboard(user.asBukkitPlayer(), StringColor.color("&9-&l-A-&r-&b-zisaba &9-&l-N-&r-&b-etwork"));
 
-		for(Quadruple<Function<UserSetting, Boolean>, Integer, Function<Localizer, String>, Function<User, Object>> line : LINES){
+		for(Quadruple<Function<UserSetting, Boolean>, Integer, String, Function<User, Object>> line : LINES){
 			//表示しない設定であれば処理しない
 			if(!line.first.apply(setting)) continue;
 
 			//表示するテキストを作成する
-			String text = StringTemplate.apply(line.third.apply(user.localizer), line.fourth.apply(user));
+			String text = user.localizer.applyAll(line.third, line.fourth.apply(user));
+			System.out.println(line.third + " : " + line.fourth.apply(user));
 
 			//対応したスコアにテキストをセットする
-			board.setScore(line.second.intValue(), text);
+			board.setScore(line.second, text);
 		}
 
 		board.setDisplay(true);
@@ -119,13 +118,13 @@ public class InformationBoard {
 	private void updateValue(int score){
 		if(board == null) return;
 
-		Quadruple<Function<UserSetting, Boolean>, Integer, Function<Localizer, String>, Function<User, Object>> line = LINES.get(score);
+		Quadruple<Function<UserSetting, Boolean>, Integer, String, Function<User, Object>> line = LINES.get(score);
 
 		//表示しない設定であれば戻る
 		if(!line.first.apply(user.setting)) return;
 
 		//表示するテキストを作成する
-		String text = StringTemplate.apply(line.third.apply(user.localizer), line.fourth.apply(user));
+		String text = user.localizer.applyAll(line.third, line.fourth.apply(user));
 
 		//指定されたスコアをアップデートする
 		board.updateScore(line.second, text);
