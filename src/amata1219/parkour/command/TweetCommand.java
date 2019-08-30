80c77e15356c702aa6eb50e8476ac5cbf6953cee
amata1219 <suzukiamata@gmail.com>
2019-08-30 17:35:55 +0900

    こみっとー

--- src/amata1219/parkour/command/TweetCommand.java
+++ src/amata1219/parkour/command/TweetCommand.java
@@ -2,20 +2,24 @@ package amata1219.parkour.command;
 
 import org.bukkit.entity.Player;
 
-import amata1219.parkour.string.message.MessageColor;
+import amata1219.parkour.text.BilingualText;
 import amata1219.parkour.tweet.Tweet;
 
 public class TweetCommand implements Command {
 
 	@Override
 	public void onCommand(Sender sender, Arguments args) {
+		//送信者がプレイヤーでなければ戻る
 		if(blockNonPlayer(sender)) return;
 
 		Player player = sender.asPlayerCommandSender();
 
-		//引数が無ければ戻る
+		//引数が指定されていなければ戻る
 		if(!args.hasNext()){
-			MessageColor.color("&c-Execution blocked &7-@ &c-Text can not be empty").displayOnActionBar(player);
+			BilingualText.stream("&c-呟く文章を入力して下さい", "&c-Please enter the text you wish to tweet")
+			.color()
+			.setReceiver(player)
+			.sendActionBarMessage();
 			return;
 		}
 
