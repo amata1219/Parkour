790de2c5c662f2bed89b6e5fa9a05117a6073895
amata1219 <suzukiamata@gmail.com>
2019-09-10 20:40:33 +0900

    こみっとー

--- src/amata1219/parkour/command/NewParkourEditCommand.java
+++ src/amata1219/parkour/command/NewParkourEditCommand.java
@@ -8,12 +8,11 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
-import amata1219.parkour.parkour.Parkour;
 import amata1219.parkour.parkour.ParkourSet;
 import amata1219.parkour.selection.RegionSelectionSet;
-import amata1219.parkour.text.Text;
 import graffiti.Command;
 import graffiti.SafeCast;
+import graffiti.Text;
 
 public class NewParkourEditCommand implements Command {
 
@@ -22,10 +21,10 @@ public class NewParkourEditCommand implements Command {
 	@Override
 	public void onCommand(CommandSender sender, String[] args) {
 		unit(SafeCast.down(sender, Player.class)).flatBind(
-			p -> unit(args[0]).bind(
-			n -> ParkourSet.getInstance().getParkour(n)).ifJust(
+			p -> unit(args[0]).flatBind(
+			n -> unit(ParkourSet.getInstance().getParkour(n)).ifJust(
 			c -> execute(p, n)
-		)).ifNothing(this::failure);
+		))).ifNothing(this::failure);
 	}
 
 	public void execute(Player player, String parkourName){
@@ -36,15 +35,11 @@ public class NewParkourEditCommand implements Command {
 		ItemStack selectionTool = selections.makeNewSelectionTool(uuid);
 		player.getInventory().addItem(selectionTool);
 
-		Text.stream("$parkour-&r-用の範囲選択ツールを与えました。")
-		.setAttribute("$parkour", parkourName)
-		.color()
-		.setReceiver(player)
-		.sendChatMessage();
+		Text.of("%s-&r-用の範囲選択ツールを与えました。").format(parkourName).accept(player::sendMessage);
 	}
 
 	private void failure(){
-
+		Text.of("&c-/parkouredit [アスレ名]");
 	}
 
 }
